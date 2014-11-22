8cdaa2e7f6b53559e8bfd57c70763a130168f0ec
Cerdic <cedric@yterium.com>
2014-11-22 17:13:35 +0100

    On ne supprime pas le type de document md quand on desinstalle car on n'est pas sur qu'il n'y etait pas avant l'installation du plugin

--- markdown_administrations.php
+++ markdown_administrations.php
@@ -25,7 +25,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function markdown_upgrade($nom_meta_base_version, $version_cible) {
 	$maj = array();
 
-	$maj['create'] = array(array('gerer_type_document_md'));
+	$maj['create'] = array(array('markdown_type_document_md'));
 
 	include_spip('base/upgrade');
 	maj_plugin($nom_meta_base_version, $version_cible, $maj);
@@ -41,30 +41,23 @@ function markdown_upgrade($nom_meta_base_version, $version_cible) {
 **/
 function markdown_vider_tables($nom_meta_base_version) {
 
-	gerer_type_document_md("retirer");
-
 	effacer_meta($nom_meta_base_version);
 }
 
 
 /**
- * Ajouter ou retirer la ligne pour les fichiers Markdown dans la table des types de document `spip_types_documents`
+ * Ajouter la ligne pour les fichiers Markdown dans la table des types de document `spip_types_documents`
  *
  * Informations sur le mime type tirées de Shared Mime Info http://cgit.freedesktop.org/xdg/shared-mime-info/tree/freedesktop.org.xml.in#n5433
  *
- * @param string $action
- *     `ajouter` (défaut) ou `retirer`
  * @return void
  */
-function gerer_type_document_md($action='ajouter') {
+function markdown_type_document_md() {
 
 	include_spip('base/abstract_sql');
 
 	// ajouter la ligne
-	if (
-		$action == 'ajouter'
-		AND !sql_countsel("spip_types_documents", array("extension=`md`"))
-	) {
+	if (!sql_countsel("spip_types_documents", "extension=".sql_quote('md'))) {
 		sql_insertq("spip_types_documents", array(
 			"extension"    => "md",
 			"titre"        => "Markdown document",
@@ -73,14 +66,6 @@ function gerer_type_document_md($action='ajouter') {
 			"upload"       => "oui",
 			"media_defaut" => "file"
 		));
-	}
-
-	// ou retirer la ligne
-	else if (
-		$action == 'retirer'
-		AND sql_countsel("spip_types_documents", array("extension=`md`"))
-	) {
-		sql_delete("spip_types_documents", "extension=`md`");
 	}
 
 }
